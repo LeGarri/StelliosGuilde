0ab4cf6dd1a13aad354c1f6fb5f50f729d8aa02f
Stree <Stree@FixeMelvin.home>
2021-04-09 17:56:34 +0200

    Ajout aide a la commande

--- src/fr/stellios/guilde/commands/Guilde.java
+++ src/fr/stellios/guilde/commands/Guilde.java
@@ -1,10 +1,14 @@
 package fr.stellios.guilde.commands;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 
-public class Guilde implements CommandExecutor {
+public class Guilde implements CommandExecutor, TabCompleter {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String msg, String[] args) {
@@ -18,6 +22,23 @@ public class Guilde implements CommandExecutor {
 		
 		return false;
 	}
+	
+	@Override
+    public List<String> onTabComplete(CommandSender sender, Command cmd, String msg, String[] args) {
+        if(cmd.getName().equalsIgnoreCase("guilde")) {
+            List<String> tab = new ArrayList<String>();
+
+            //commande /guilde ou /gl
+            if(args.length == 1) {
+                tab.add("help");
+
+                return tab;
+            }
+
+        }
+
+        return null;
+    }
 	
 	private void showHelpMessage(CommandSender sender) {
 		sender.sendMessage("§6/guilde create §e<Nom de la Guilde> §6- §eCréer votre guilde");
